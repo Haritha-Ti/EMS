24a29d42f0dfb95c4a7dc16d5238ae4577ae9ce6
Nisha Thomas <nisha.thomas@titechnologies.org>
2019-10-01 14:57:19 +0530

    prev log check api change

--- src/main/java/com/EMS/controller/TasktrackController.java
+++ src/main/java/com/EMS/controller/TasktrackController.java
@@ -2759,7 +2759,7 @@ public class TasktrackController {
 	}
 	
 	/***
-	 * @des approved datas from level2 populates to finance table
+	 * @des approved datas from level1 populates to finance table
 	 * @param requestdata
 	 * @param httpstatus
 	 * @return
