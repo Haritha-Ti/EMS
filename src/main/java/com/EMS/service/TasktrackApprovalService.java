a55c0328ac072b291acdbb8b78b4a7efc0b7e392
sreejithjagadeesan <sreejith.j@RCG.IND.COM>
2019-11-07 16:46:52 +0530

    Added Api's for save and submit from the approver screen for a 2 tier project

--- src/main/java/com/EMS/service/TasktrackApprovalService.java
+++ src/main/java/com/EMS/service/TasktrackApprovalService.java
@@ -8,37 +8,36 @@ import java.util.List;
 import java.util.Set;
 
 import org.json.simple.JSONObject;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 
-import com.EMS.model.AllocationModel;
-import com.EMS.model.ProjectModel;
-import com.EMS.model.Task;
 import com.EMS.model.TaskTrackApproval;
 import com.EMS.model.TaskTrackApprovalFinance;
 import com.EMS.model.TaskTrackApprovalLevel2;
-import com.EMS.model.Tasktrack;
-import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 
 public interface TasktrackApprovalService {
 
 	Boolean checkIsUserExists(Long id);
-	
+
 	List<JSONObject> getTimeTrackUserTaskDetails(Long id, Date startDate, Date endDate, List<Object[]> userList,
-			List<JSONObject> loggedJsonArray,List<JSONObject> billableJsonArray,List<JSONObject> timeTrackJSONData, Boolean isExist,Long projectId);
-	List<JSONObject> getTimeTrackUserTaskDetailsByProject(Long id, Date startDate, Date endDate, List<Object[]> userList,
-												 List<JSONObject> loggedJsonArray,List<JSONObject> billableJsonArray,List<JSONObject> nonBillableJsonArray,List<JSONObject> timeTrackJSONData, Boolean isExist,Long projectId);
-	List<JSONObject> getTimeTrackUserProjectTaskDetails(Long projectId,String projectName, Date startDate, Date endDate, List<Object[]> projectList,
-												 List<JSONObject> loggedJsonArray,List<JSONObject> billableJsonArray,List<JSONObject> nonBillableJsonArray,List<JSONObject> timeTrackJSONData, Boolean isExist,Long userId);
-	
+			List<JSONObject> loggedJsonArray, List<JSONObject> billableJsonArray, List<JSONObject> timeTrackJSONData,
+			Boolean isExist, Long projectId);
+
+	List<JSONObject> getTimeTrackUserTaskDetailsByProject(Long id, Date startDate, Date endDate,
+			List<Object[]> userList, List<JSONObject> loggedJsonArray, List<JSONObject> billableJsonArray,
+			List<JSONObject> nonBillableJsonArray, List<JSONObject> timeTrackJSONData, Boolean isExist, Long projectId);
+
+	List<JSONObject> getTimeTrackUserProjectTaskDetails(Long projectId, String projectName, Date startDate,
+			Date endDate, List<Object[]> projectList, List<JSONObject> loggedJsonArray,
+			List<JSONObject> billableJsonArray, List<JSONObject> nonBillableJsonArray,
+			List<JSONObject> timeTrackJSONData, Boolean isExist, Long userId);
+
 	JSONObject getApprovedUserTaskDetails(Long id, Date startDate, Date endDate, List<TaskTrackApproval> userList,
-			List<JSONObject> jsonArray, List<JSONObject> jsonDataRes1, Boolean isExist,Long projectId);
-	
+			List<JSONObject> jsonArray, List<JSONObject> jsonDataRes1, Boolean isExist, Long projectId);
+
 	TaskTrackApproval findById(Long billableId);
-	
+
 	public TaskTrackApproval updateData(TaskTrackApproval taskTrackApproval);
-	
+
 	public TaskTrackApproval save(TaskTrackApproval taskTrackApproval);
 
 	TaskTrackApprovalLevel2 findById2(Long billableId);
@@ -50,61 +49,71 @@ public interface TasktrackApprovalService {
 	JSONObject getApproveddatalevel2(Long userId, Date startDate, Date endDate, List<TaskTrackApproval> userList,
 			List<JSONObject> jsonArray, List<JSONObject> approvalJSONData, Boolean isExist, Long projectId);
 
-
 	void saveLevel3(TaskTrackApprovalFinance taskTrackApproval);
 
-	JSONObject getApproveddatalevel2toFinance(Long userId, Long logUser,int mothIndex,  int yearIndex, Long projectId);
+	JSONObject getApproveddatalevel2toFinance(Long userId, Long logUser, int mothIndex, int yearIndex, Long projectId);
 
-	JSONObject getApproveddatalevel1toFinance(Long userId, Long logUser,int monthIndex, int yearIndex, Long projectId);
+	JSONObject getApproveddatalevel1toFinance(Long userId, Long logUser, int monthIndex, int yearIndex, Long projectId);
 
 	List<JSONObject> getTimeTrackUserTaskDetailsLevel2(Long id, Date startDate, Date endDate, List<Object[]> userList,
 			List<JSONObject> loggedJsonArray, List<JSONObject> billableJsonArray, List<JSONObject> timeTrackJSONData,
 			Boolean isExist, Long projectId);
 
-	List<Object> getForwardedDate(Long projectId, Long userId, int intMonth,int year);
+	List<Object> getForwardedDate(Long projectId, Long userId, int intMonth, int year);
 
-	List<Object> getForwardedDateLevel2(Long projectId, Long userId, int intMonth,int year);
+	List<Object> getForwardedDateLevel2(Long projectId, Long userId, int intMonth, int year);
 
-	ObjectNode saveLevel2FromLevel1(Long projectId, Long userId,Long logUser, Date startDate, Date endDate);
+	ObjectNode saveLevel2FromLevel1(Long projectId, Long userId, Long logUser, Date startDate, Date endDate);
 
 	List<Object[]> getForwardedDates(Long projectId, Long userId, int intMonth, int yearIndex);
 
 	List<TaskTrackApprovalLevel2> getUserIdByProjectAndDateForLevel2(Long projectId, Date startDate, Date endDate);
-
 
 	/*
 	 * JSONObject getApprovedUserTaskDetailsForLevel2(Long userId, Date startDate,
 	 * Date endDate, List<TaskTrackApproval> userList, List<JSONObject> jsonArray,
 	 * List<JSONObject> approvalJSONData, Boolean isExist, Long projectId);
 	 */
-	public ArrayList<JSONObject> getFinanceDataByProject( int month, int year, Long projectId);
+	public ArrayList<JSONObject> getFinanceDataByProject(int month, int year, Long projectId);
 
-	public ArrayList<JSONObject> getFinanceDataByUser( int month, int year, Long userId);
+	public ArrayList<JSONObject> getFinanceDataByUser(int month, int year, Long userId);
 
-	public ArrayList<JSONObject> getFinanceDataByUserAndProject(int month, int year,Long userId, Long projectId);
-
+	public ArrayList<JSONObject> getFinanceDataByUserAndProject(int month, int year, Long userId, Long projectId);
 
 	JSONObject checkPreviousTimeSheetsareClosed(int month, int year, Long projectId, Long userId);
+
 	JSONObject halfCycleCheck(Long projectId, Long userId, Long approverId, Date endDate) throws ParseException;
 
 	Object[] getFinanceStatusOfCurrentProject(Long projectId, Long userId, int intMonth, int yearIndex);
 
-	ObjectNode reApproveDatasofLevel1(Long projectId, Long userId, int month, int year,HashMap<String, Object> billableArray,HashMap<String, Object> nonbillableArray,HashMap<String, Object> beachArray,HashMap<String, Object> overtimeArray,Long billableId,Long nonbillableId,Long overtimeId,Long beachId,Long logUser);
+	ObjectNode reApproveDatasofLevel1(Long projectId, Long userId, int month, int year,
+			HashMap<String, Object> billableArray, HashMap<String, Object> nonbillableArray,
+			HashMap<String, Object> beachArray, HashMap<String, Object> overtimeArray, Long billableId,
+			Long nonbillableId, Long overtimeId, Long beachId, Long logUser);
 
 	ObjectNode reApproveDatasofLevel2(Long projectId, Long userId, Integer month, Integer year,
 			HashMap<String, Object> billableArray, HashMap<String, Object> nonbillableArray,
 			HashMap<String, Object> beachArray, HashMap<String, Object> overtimeArray, Long billableId,
 			Long nonbillableId, Long overtimeId, Long beachId, Long logUser);
 
-	ObjectNode mailRejectTimesheetDetailstoLevel1andClear(Long projectId, Long userId, Long month, Long year,String message);
+	ObjectNode mailRejectTimesheetDetailstoLevel1andClear(Long projectId, Long userId, Long month, Long year,
+			String message);
 
 	ArrayList<JSONObject> getFinanceDataByMonthAndYear(int month, int year);
-	
-	             //Renjith
-	        List<TaskTrackApprovalLevel2> getNotApprovedData( int  monthIndex, int yearIndex, Long projectId);
-	        List<TaskTrackApprovalLevel2> getHalfMonthApprovedData( int  monthIndex, int yearIndex, Long projectId);
-	        public ArrayList<JSONObject>  getFinanceDataByProjectSet(int month, int year, Set<Long> id);
-	            //Renjith
-		    List<TaskTrackApprovalLevel2> getMidMonthApprovedData(int monthIndex, int yearIndex, Long projectId);
-}
 
+	// Renjith
+	List<TaskTrackApprovalLevel2> getNotApprovedData(int monthIndex, int yearIndex, Long projectId);
+
+	List<TaskTrackApprovalLevel2> getHalfMonthApprovedData(int monthIndex, int yearIndex, Long projectId);
+
+	public ArrayList<JSONObject> getFinanceDataByProjectSet(int month, int year, Set<Long> id);
+
+	// Renjith
+	List<TaskTrackApprovalLevel2> getMidMonthApprovedData(int monthIndex, int yearIndex, Long projectId);
+
+	void saveApprovedHours(JSONObject requestData) throws Exception;
+
+	void submitFirstHalfHoursForApproval(JSONObject requestData) throws Exception;
+
+	void submitSecondHalfHoursForApproval(JSONObject requestData) throws Exception;
+}
