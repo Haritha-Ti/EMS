6e0c1a03f2814c66cdc7298c4da9fef2091a280a
sreejithjagadeesan <sreejith.j@RCG.IND.COM>
2019-12-12 15:38:29 +0530

    Added method for fetching active allocation count

--- src/main/java/com/EMS/repository/AllocationRepository.java
+++ src/main/java/com/EMS/repository/AllocationRepository.java
@@ -1,8 +1,11 @@
 package com.EMS.repository;
 
 import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.EMS.model.AllocationModel;
 
@@ -10,4 +13,7 @@ public interface AllocationRepository extends JpaRepository<AllocationModel, Lon
 
 	public AllocationModel findOneByProjectProjectIdAndUserUserIdAndStartDateLessThanEqualAndEndDateGreaterThanEqualAndIsBillableAndActiveOrderByEndDateDesc(
 			Long projectId, Long userId, Date endDate, Date startDate, Boolean isBillable, Boolean isActive);
+
+	@Query(value = "select project_project_id as projectId, count(alloc_id) as count from allocation where (?1 between start_date and end_date) group by project_project_id", nativeQuery = true)
+	public Object[][] getActiveAllocationCountByProjects(String currentDate);
 }
