b87981ece643814e702f12ccbe33e4c6685925ed
Suhail <suhail.najeem@rcggs.com>
2020-01-09 18:50:39 +0530

    weekly approval with task

--- src/main/java/com/EMS/repository/TaskWeeklyApprovalRepository.java
+++ src/main/java/com/EMS/repository/TaskWeeklyApprovalRepository.java
@@ -1,6 +1,7 @@
 package com.EMS.repository;
 
 import java.util.Date;
+import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -13,5 +14,11 @@ public interface TaskWeeklyApprovalRepository extends JpaRepository<TaskTrackWee
 	@Query(value = "SELECT ta FROM TaskTrackWeeklyApproval AS ta \n" + 
 			" WHERE ta.startDate = :startDate and ta.endDate = :endDate and ta.user.userId = :userId and ta.project.projectId = :projectId")
 	public TaskTrackWeeklyApproval getWeeklyTasktrack(@Param("startDate") Date startDate, @Param("endDate") Date endDate, @Param("userId") long userId, @Param("projectId") long projectId) throws Exception;
+	
+	@Query(value = "select ta.task_name, tt.date, tt.hours, tt.description \n" + 
+			"from task_master ta left join tasktrack tt\n" + 
+			"on ta.id = tt.task_id\n" + 
+			"", nativeQuery = true)
+	public List<Object[]> getWeeklyTasktrackWithTask();
 	
 }
