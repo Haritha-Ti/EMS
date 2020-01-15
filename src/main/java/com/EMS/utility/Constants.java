fabf6c9fdfab329ce1f9007ed13a4d447c51308a
charlyjoseph <charly.joseph@rcggs.com>
2020-01-15 17:23:34 +0530

    Cahnges in semiMonthely Controller

--- src/main/java/com/EMS/utility/Constants.java
+++ src/main/java/com/EMS/utility/Constants.java
@@ -83,4 +83,7 @@ public class Constants {
 	public static final String SUCCESS = "Success";
 	public static final String OK = "OK";
 	public static final String ERROR = "Error";
+	public static final String NO_DATA_FOUND_MESSAGE = "No Data Available";
+
+	
 }
