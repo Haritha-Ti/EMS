c94764d307f29e2ee25949ce6774aca0385d0036
Drishya-d <drishya.dinesh@rcggs.com>
2020-01-17 17:06:26 +0530

    approver1 and approver2 names and resubmission bug solv

--- src/main/java/com/EMS/service/TasktrackApprovalServiceImpl.java
+++ src/main/java/com/EMS/service/TasktrackApprovalServiceImpl.java
@@ -11198,6 +11198,8 @@ public class TasktrackApprovalServiceImpl implements TasktrackApprovalService {
 		ProjectModel projectData = projectRepository.getProjectDetails(projectId);
 		Boolean submitButtonStatus = Boolean.FALSE;
 		Boolean rejectButtonStatus = Boolean.FALSE;
+		String projectApprover1 = projectData.getProjectOwner().getLastName()+" "+projectData.getProjectOwner().getFirstName();
+		String projectApprover2 = projectData.getOnsite_lead().getLastName()+" "+projectData.getOnsite_lead().getFirstName();
 		if (projectData != null) {
 			ArrayNode hourDataNode = objectMapper.createArrayNode();
 			// weekly
@@ -11253,6 +11255,8 @@ public class TasktrackApprovalServiceImpl implements TasktrackApprovalService {
 					hourDataResponse.put(df.format(cal.getTime()),userData.getApprover1Status() != null?(userData.getApprover1Status().equals(Constants.TASKTRACK_APPROVER1_STATUS_FORWARDED_TO_LEVEL2) ? userData.getDay7():0):0);
 					hourDataNode.add(hourDataResponse);
 					node.set("hourData", hourDataNode);
+					node.put("approver1Name",userData.getApprover1Id() == null ? projectApprover1 :(userData.getApprover1Id().getLastName() + " "+ userData.getApprover1Id().getFirstName()));
+					node.put("approver2Name",userData.getApprover2Id() == null ? projectApprover2 :(userData.getApprover2Id().getLastName() + " "+ userData.getApprover2Id().getFirstName()));
 				} else {
 					UserModel user = userRepository.findOneByUserId(userId);
 					if (user != null) {
@@ -11267,9 +11271,13 @@ public class TasktrackApprovalServiceImpl implements TasktrackApprovalService {
 
 							node.put("approver2Status", Constants.TASKTRACK_APPROVER_STATUS_OPEN);
 							node.put("approver2SubmittedDate", "");
+							node.put("approver1Name", projectApprover1);
+							node.put("approver2Name", projectApprover2);
 						} else {
 							node.put("approver2Status", "");
 							node.put("approver2SubmittedDate", "");
+							node.put("approver1Name", projectApprover1);
+							node.put("approver2Name", "");
 						}
 						ObjectNode hourDataResponse = objectMapper.createObjectNode();
 						Calendar cal = Calendar.getInstance();
@@ -11375,7 +11383,8 @@ public class TasktrackApprovalServiceImpl implements TasktrackApprovalService {
 						}
 						node.put("submitButtonStatus", submitButtonStatus);
 						node.put("rejectButtonStatus", rejectButtonStatus);
-
+						node.put("approver1Name",userData.getFirstHalfApproverOneId() == null ?projectApprover1:(userData.getFirstHalfApproverOneId().getLastName()+" "+userData.getFirstHalfApproverOneId().getFirstName()));
+						node.put("approver2Name",userData.getFirstHalfApproverTwoId() == null ?projectApprover2:(userData.getFirstHalfApproverTwoId().getLastName()+" "+userData.getFirstHalfApproverTwoId().getFirstName()));
 					} else {
 
 						hourDataResponse.put(df.format(cal.getTime()), userData.getApproverOneFirstHalfStatus() != null?(userData.getApproverOneFirstHalfStatus().equals(Constants.TASKTRACK_APPROVER1_STATUS_FORWARDED_TO_LEVEL2) ? userData.getDay16():0):0);
@@ -11443,6 +11452,8 @@ public class TasktrackApprovalServiceImpl implements TasktrackApprovalService {
 					}
 					node.put("submitButtonStatus", submitButtonStatus);
 					node.put("rejectButtonStatus", rejectButtonStatus);
+					node.put("approver1Name",userData.getSecondHalfApproverOneId() == null ?projectApprover1:(userData.getSecondHalfApproverOneId().getLastName()+" "+userData.getSecondHalfApproverOneId().getFirstName()));
+					node.put("approver2Name",userData.getSecondHalfApproverTwoId() == null ?projectApprover2:(userData.getSecondHalfApproverTwoId().getLastName()+" "+userData.getSecondHalfApproverTwoId().getFirstName()));
 					hourDataNode.add(hourDataResponse);
 					node.set("hourData", hourDataNode);
 				} else {
@@ -11539,6 +11550,8 @@ public class TasktrackApprovalServiceImpl implements TasktrackApprovalService {
 						node.set("hourData", hourDataNode);
 						node.put("submitButtonStatus", false);
 						node.put("rejectButtonStatus", false);
+						node.put("approver1Name",projectApprover1);
+						node.put("approver2Name", projectApprover2);
 					}
 				}
 			}
