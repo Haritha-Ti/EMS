bd361bda13d874e6a2b049f3f40970d1a95c4ce3
Muhammed Hashir <hashir.k@RCG.IND.COM>
2020-01-20 13:03:13 +0530

    Close rejections

--- src/main/java/com/EMS/repository/TaskTrackRejectionRepository.java
+++ src/main/java/com/EMS/repository/TaskTrackRejectionRepository.java
@@ -4,7 +4,9 @@ import java.util.Date;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.EMS.model.TaskTrackRejection;
 import com.EMS.utility.Constants;
@@ -25,13 +27,32 @@ public interface TaskTrackRejectionRepository extends JpaRepository<TaskTrackRej
 	List<TaskTrackRejection> findOpenRejectionForUserForProject(Long userId, Long projectId, Integer month,Integer year);
 
 	
+	
+	
 	@Query("select rejection FROM TaskTrackRejection rejection "
-			+ "where rejection.user.userId = ?1 and rejection.project.projectId = ?2 and rejection.year = ?3 "
-			+ "and rejection.startDate = ?4 and rejection.endDate = ?5 and rejection.status = ?6 ")
-	TaskTrackRejection findWeeklyRejection(Long userId, Long projectId, Integer year, Date startDate,Date endDate,String status);
+			+ "where rejection.user.userId = ?1 and rejection.project.projectId = ?2 "
+			+ "and rejection.startDate = ?3 and rejection.endDate = ?4 and rejection.status = ?5 "
+			+ "and rejection.rejectionLevel = ?6")
+	TaskTrackRejection findWeeklyRejection(Long userId, Long projectId, Date startDate,Date endDate,String status,Integer rejectionLevel);
 	
 	@Query("select rejection FROM TaskTrackRejection rejection "
 			+ "where rejection.user.userId = ?1 and rejection.project.projectId = ?2 and rejection.month = ?3 "
-			+ "and rejection.year = ?4 and rejection.status = ?5 ")
-	List<TaskTrackRejection> findSemiMonthlyRejection(Long userId, Long projectId, Integer month, Integer year,String status);
+			+ "and rejection.year = ?4 and rejection.status = ?5 "
+			+ "and rejection.rejectionLevel = ?6")
+	List<TaskTrackRejection> findSemiMonthlyRejection(Long userId, Long projectId, Integer month, Integer year,String status,Integer rejectionLevel);
+	
+	@Transactional
+	@Modifying
+	@Query("Update TaskTrackRejection rejection set rejection.status = '"+Constants.TASKTRACK_REJECTION_STATUS_CLOSED+"' "
+			+ "where rejection.user.userId = ?1 and rejection.project.projectId = ?2 "
+			+ "and rejection.startDate = ?3 and rejection.endDate = ?4 and rejection.rejectionLevel = ?5")
+	int closeWeeklyRejection(Long userId, Long projectId, Date startDate,Date endDate,Integer rejectionLevel);
+	
+	@Transactional
+	@Modifying
+	@Query("Update TaskTrackRejection rejection set rejection.status = '"+Constants.TASKTRACK_REJECTION_STATUS_CLOSED+"' "
+			+ "where rejection.user.userId = ?1 and rejection.project.projectId = ?2 and rejection.month = ?3 "
+			+ "and rejection.year = ?4 and rejection.cycle = ?5 and rejection.rejectionLevel = ?6 ")
+
+	int closeSemiMonthlyRejection(Long userId, Long projectId, Integer month, Integer year,String cycle,Integer rejectionLevel);
 }
