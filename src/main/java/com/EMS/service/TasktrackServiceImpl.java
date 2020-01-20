bd0769ecd2e0c1fd36483a644a2abf4712629a0a
charlyjoseph <charly.joseph@rcggs.com>
2020-01-20 12:34:04 +0530

    Changes in tasktrack service

--- src/main/java/com/EMS/service/TasktrackServiceImpl.java
+++ src/main/java/com/EMS/service/TasktrackServiceImpl.java
@@ -1042,7 +1042,7 @@ public class TasktrackServiceImpl implements TasktrackService {
 
 				if (projHalfs.containsKey(String.valueOf(modelObj.getProject().getProjectId()) + "F")) {
 					firstHalfObj.put("hours", firstHalfHours);
-					firstHalfObj.put("status", firstHalfFinalStatus);
+					firstHalfObj.put("status", firstHalfFinalStatus == null ? "OPEN" : firstHalfFinalStatus.toUpperCase());
 					firstHalfObj.put("startDay", dateFrmt.format(monthStartCal.getTime()));
 					firstHalfObj.put("endDay", year + "-" + month + "-" + "15");
 					periodsArray.add(firstHalfObj);
@@ -1050,7 +1050,7 @@ public class TasktrackServiceImpl implements TasktrackService {
 				}
 				if (projHalfs.containsKey(String.valueOf(modelObj.getProject().getProjectId()) + "S")) {
 					secondHalfObj.put("hours", secondHalfHours);
-					secondHalfObj.put("status", secondHalfFinalStatus);
+					secondHalfObj.put("status", secondHalfFinalStatus == null ? "OPEN" : secondHalfFinalStatus.toUpperCase());
 					secondHalfObj.put("startDay", year + "-" + month + "-" + "16");
 					secondHalfObj.put("endDay", dateFrmt.format(monthEndCal.getTime()));
 					periodsArray.add(secondHalfObj);
@@ -1137,14 +1137,14 @@ public class TasktrackServiceImpl implements TasktrackService {
 
 				if (hoursObj.containsKey("firstHalfHours")) {
 					firstHalfObj.put("hours", hoursObj.get("firstHalfHours"));
-					firstHalfObj.put("status", modelObj.getFirstHalfFinalStatus());
+					firstHalfObj.put("status", modelObj.getFirstHalfFinalStatus() == null ? "OPEN": modelObj.getFirstHalfFinalStatus().toUpperCase());
 					firstHalfObj.put("startDay", dateFrmt.format(monthStartCal.getTime()));
 					firstHalfObj.put("endDay", year + "-" + month + "-" + "15");
 					periodsArray.add(firstHalfObj);
 				}
 				if (hoursObj.containsKey("secondHalfHours")) {
 					secondHalfObj.put("hours", hoursObj.get("secondHalfHours"));
-					secondHalfObj.put("status", modelObj.getSecondHalfFinalStatus());
+					secondHalfObj.put("status", modelObj.getSecondHalfFinalStatus() == null ? "OPEN": modelObj.getSecondHalfFinalStatus().toUpperCase());
 					secondHalfObj.put("startDay", year + "-" + month + "-" + "16");
 					secondHalfObj.put("endDay", dateFrmt.format(monthEndCal.getTime()));
 					periodsArray.add(secondHalfObj);
