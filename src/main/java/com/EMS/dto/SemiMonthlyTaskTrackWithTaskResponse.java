5efde7f885821267760cf46693e66cb45b99ef8e
Haritha Unni <haritha.unni@rcggs.com>
2020-01-26 23:00:44 +0530

    new response dto

---
+++ src/main/java/com/EMS/dto/SemiMonthlyTaskTrackWithTaskResponse.java
@@ -0,0 +1,47 @@
+package com.EMS.dto;
+
+import java.util.List;
+
+import org.json.simple.JSONObject;
+
+public class SemiMonthlyTaskTrackWithTaskResponse {
+
+	private String date;
+	private Boolean enabled;
+	private Double finalHour;
+	private List<TasktrackDto> taskList;
+
+	public String getDate() {
+		return date;
+	}
+
+	public void setDate(String date) {
+		this.date = date;
+	}
+
+	public Boolean getEnabled() {
+		return enabled;
+	}
+
+	public void setEnabled(Boolean enabled) {
+		this.enabled = enabled;
+	}
+
+	public Double getFinalHour() {
+		return finalHour;
+	}
+
+	public void setFinalHour(Double finalHour) {
+		this.finalHour = finalHour;
+	}
+
+	public List<TasktrackDto> getTaskList() {
+		return taskList;
+	}
+
+	public void setTaskList(List<TasktrackDto> taskList) {
+		this.taskList = taskList;
+	}
+	
+	
+}
